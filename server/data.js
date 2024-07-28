e781e5a435618dd41f425b5d3174c6d5b70cde7d
ayan878 <ayanraza197@gmail.com>
2024-07-28 16:54:30 +0530

    dummy data

---
+++ server/data.js
@@ -0,0 +1,7 @@
+// Define dummy data
+export const dummyTodos = [
+  { title: "Buy groceries", completed: false },
+  { title: "Read a book", completed: true },
+  { title: "Go to the gym", completed: false },
+  { title: "Write a blog post", completed: true },
+];
