b4654a2d43978d2897de089399684e6b5dde9e52
ayan878 <ayanraza197@gmail.com>
2024-07-29 01:53:43 +0530

    model

--- server/src/models/TodoModel.js
+++ server/src/models/TodoModel.js
@@ -3,10 +3,15 @@ import mongoose from "mongoose";
 const TodoSchema = new mongoose.Schema({
   title: {
     type: String,
+    require: [true, "Please enter the title"],
   },
   completed: {
     type: Boolean,
     default: false,
+  },
+  desc: {
+    type: String,
+    require: [true, "Please enter the Description"],
   },
 });
 
